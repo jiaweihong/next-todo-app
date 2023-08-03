29e7f4bcc68e4bceeecc14146db6ed959a601073
Jia Wei Hong <jiaweihong1@hotmail.com>
2023-08-03 12:05:02 +0800

    configure firebase db to app

---
+++ firebase.js
@@ -0,0 +1,15 @@
+import { initializeApp } from 'firebase/app';
+import {getAuth} from 'firebase/auth';
+import {getfireStore} from 'firebase/firestore'
+
+// TODO: Replace the following with your app's Firebase project configuration
+const firebaseConfig = {
+    apiKey: process.env.API_KEY,
+    authDomain: process.env.AUTH_DOMAIN,
+    projectId: process.env.PROJECT_ID,
+    storageBucket: process.env.STORAGE_BUCKET,
+    messagingSenderId: process.env.MESSAGING_SENDER_ID,
+    appId: process.env.APP_ID
+};
+
+const app = initializeApp(firebaseConfig);
